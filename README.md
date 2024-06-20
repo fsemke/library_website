c705f6c56f20770723e40d08ef15721e9ac757a1
fsemke <fsemke@student.42wolfsburg.de>
2024-06-20 13:28:00 +0200

    added project

---
+++ README.md
@@ -0,0 +1,86 @@
+# Library 1.0
+#### Video Demo: https://www.youtube.com/watch?v=ZAGGfjAvzlE
+#### Description:
+This website is a conceptual solution for organizing our small club library. Every member of the club has free access to it, and the system operates on a trust basis. Currently, the library is managed using a traditional index card system. The goal of this website is to modernize and simplify the process of borrowing and returning books.
+
+# Functionality
+
+## Register / Login
+To begin using the system, users need to register and log in. If you are the first user in the database, you will automatically be granted admin status. Our system uses token-based authentication to verify user identities and ensure secure access.
+
+## Library
+The library section allows users to browse through all the books available. The books are categorized into two sections: "Available books" and "Books on loan". Each book entry is clickable, providing more details when selected.
+
+### Admin Capabilities
+If you are an admin, you have additional capabilities. You can create a new book entry by providing all necessary details, including the title, author, published date, and a link to an image of the book cover.
+
+### Book Description
+When you click on a book, you are taken to a detailed description page. This page provides comprehensive information about the book, including its availability status. Users can borrow the book directly from this page. Admins have additional access to a borrowing history, showing which users have borrowed the book in the past.
+
+## Users
+This section lists all registered users within the system. Users can view the basic information of other members.
+
+### Admin Capabilities
+As an admin, you can manage user accounts more thoroughly. You have the ability to delete users from the system.
+
+### Specific User Profile
+On your own profile page, you can see a list of books you currently have borrowed. You also have the option to set a new password for your account. If you are an admin, you can view the borrowed books of any user, reset passwords for any account, and upgrade or downgrade user privileges to admin status.
+
+## Statistics
+Admins have access to a dedicated statistics section. Here, you can view a list of books that have been borrowed for the longest periods. This feature helps ensure that no book remains borrowed for an excessively long time without being returned.
+
+# Files
+
+## app.py
+All the backend logic and functionality of the website are contained within this file. It handles user authentication, database interactions, and the overall operation of the website.
+
+## layout.html
+This is the base HTML file upon which all other HTML files are built. It includes the navigation bar and head configurations, ensuring a consistent look and feel across the entire website.
+
+## Login / Registration
+The login and registration pages are designed based on a template, providing a visually appealing interface for users to access the system.
+
+## Home
+There are two different HTML files for the homepage, one for logged-in users and one for visitors. The appropriate file is served based on the user's login status.
+
+## Library
+The library page uses CSS flexbox for layout management. This ensures that the display adapts seamlessly to varying numbers of books.
+
+### add_book
+This page allows admins to add new books to the library. Initially, the system checks if the user has admin privileges. If not, the user is redirected back to a previous page. Although there is no current feature to upload book covers directly, it remains a potential enhancement for future updates.
+
+### book_detail
+The book detail page includes several interactive options:
+1. If the book is available, a "Borrow the book" button is displayed.
+2. If you have borrowed the book, a "Return the book" button is shown.
+3. If another user has borrowed the book, a message stating "Book is not available" is displayed.
+
+Only admins have access to the borrowing history of the book to respect user privacy. The history is limited to the last 20 entries to keep the information concise and clear. Entries are ordered by date or, alternatively, by ID, to display them chronologically.
+
+## Statistics
+The statistics page also displays information ordered by date. The queries used to generate these statistics were complex, involving joins between multiple tables to gather all necessary data.
+
+## Users
+In the users section, a JavaScript popup is implemented to prevent accidental deletion of user accounts. This additional step ensures that user management actions are deliberate and reduces the risk of mistakes.
+
+## User
+This page displays all the books currently borrowed by a specific user.
+
+#### How It's Made
+This project is entirely coded using Flask, along with some JavaScript. The backend relies on an SQLite3 database, structured with three interconnected tables to manage users, books, and borrowing history. SQLAlchemy is used to connect Flask with the database, providing an ORM (Object Relational Mapping) layer for efficient data management.
+
+# .env
+The .env file needs two variables:
+- DATABASE_URI for example: 'sqlite:///db.db'
+- SECRET_KEY
+
+# To start the website in dev mode
+``python3 app.py``
+
+# Deploy
+## Build
+``docker build -t florian/lib-app:v1.0.0 .``
+
+## Run docker
+``docker run --name library -p 5000:5000 -v ./db:/app/instance -d florian/lib-app:v1.0.0``
+
---
+++ src/app.py
@@ -0,0 +1,312 @@
+from flask import Flask, render_template, url_for, redirect, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, LoginManager, login_user, logout_user, login_required, current_user
+from flask_bcrypt import Bcrypt
+from datetime import datetime
+from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+CORS(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+  return User.query.get(int(user_id))
+
+
+
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'user'
+     
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+    firstname = db.Column(db.String(64), nullable=False)
+    lastname = db.Column(db.String(64), nullable=False)
+    admin = db.Column(db.Boolean, nullable=False)
+
+    books = db.relationship('Book', back_populates='user', lazy=True)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+class Book(db.Model):
+    __tablename__ ='book'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128), nullable=False)
+    author = db.Column(db.String(128), nullable=False)
+    published_date = db.Column(db.Date, nullable=False)
+    img_url = db.Column(db.String(128), nullable=True)
+    
+    borrowed_from = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
+    borrowed_date = db.Column(db.Date, nullable=True)
+    user = db.relationship('User', back_populates='books')
+
+    def __repr__(self):
+        return f'<Book {self.title}>'
+
+class History(db.Model):
+    __tablename__ = 'history'
+
+    id = db.Column(db.Integer, primary_key=True)
+    action = db.Column(db.String(128), nullable=False)
+    date = db.Column(db.Date, nullable=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
+    user = db.relationship("User")
+    book_id = db.Column(db.Integer, db.ForeignKey('book.id'), nullable=True)
+    book = db.relationship("Book")
+
+
+
+@app.route("/")
+def home():
+    if current_user.is_authenticated:
+        return render_template('home_logged_in.html', loggedIn = True)
+    else:
+        return render_template('home_logged_out.html', loggedIn = False)
+
+@app.route('/library')
+@login_required
+def library():
+    available = Book.query.filter(Book.borrowed_from == None).all()
+    not_available = Book.query.filter(Book.borrowed_from != None).all()
+
+    return render_template('library.html', available=available, not_available=not_available, loggedIn = True)
+
+
+@app.route('/addbook', methods=['GET','POST'])
+@login_required
+def addBook():
+    if not current_user.admin:
+        return redirect(url_for('library'))
+    if request.method == 'POST':
+        # Create new book
+        title = request.form['title']
+        author = request.form['author']
+        imgUrl = request.form['image_url']
+        publishedDate = request.form['published_date']
+        publishedDate = datetime.strptime(publishedDate, '%Y-%m-%d')
+
+        new_book = Book(
+            title=title,
+            author=author,
+            published_date=publishedDate,
+            img_url=imgUrl)
+        db.session.add(new_book)
+        db.session.commit()
+        return redirect(url_for('library'))
+    
+    else:
+        return render_template('add_book.html', loggedIn = True)
+
+@app.route('/book/<int:book_id>', methods=['GET', 'POST'])
+@login_required
+def book_detail(book_id):
+    if request.method == 'POST':
+        user = User.query.get(current_user.id)
+
+        book = Book.query.get_or_404(book_id)
+        book.borrowed_from = user.id
+        book.borrowed_date = datetime.now()
+
+        new_history = History(
+            action="borrow",
+            date=datetime.now(),
+            user_id=user.id,
+            book_id=book.id
+            )
+        
+        db.session.add(new_history)
+        db.session.commit()
+        return redirect(url_for('book_detail', book_id=book.id))
+
+    book = Book.query.get_or_404(book_id)
+    history = db.session.query(History)\
+        .join(User, History.user_id == User.id)\
+        .filter(History.book_id == book.id)\
+        .order_by(
+            History.date.desc(),
+            History.id.desc()
+        )\
+        .limit(15)
+    return render_template('book_detail.html', book=book, history=history, loggedIn=True)
+
+@app.route('/bookreturn/<int:book_id>', methods=['POST'])
+@login_required
+def return_book(book_id):
+    book = Book.query.get_or_404(book_id)
+    if (book.borrowed_from == current_user.id):
+        book.borrowed_from = None
+        book.borrowed_date = None
+
+        new_history = History(
+            action="return",
+            date=datetime.now(),
+            user_id=current_user.id,
+            book_id=book.id
+            )
+        
+        db.session.add(new_history)
+        db.session.commit()
+    return redirect(url_for('book_detail', book_id=book.id))
+
+@app.route('/statistics')
+@login_required
+def statistic():
+    if current_user.admin:
+
+        books = db.session.query(Book)\
+        .where(Book.borrowed_from != None)\
+        .join(User, Book.borrowed_from == User.id)\
+        .order_by(Book.borrowed_date.asc())\
+        .all()
+
+        for book in books:
+            book.borrowed_days = (datetime.now().date() - book.borrowed_date).days
+
+        return render_template('statistics.html', books=books, loggedIn=True)
+    return redirect(url_for('home'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('library'))
+
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+
+        if user and bcrypt.check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for('library'))
+
+    return render_template('login.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('dashboard'))
+    
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        hashed_password = bcrypt.generate_password_hash(
+            password).decode('utf-8')
+        # If first User, add Admin privilege
+        if User.query.count()  == 0:
+            new_user = User(username=username, password=hashed_password, firstname=firstname, lastname=lastname, admin=True)
+        else:
+            new_user = User(username=username, password=hashed_password, firstname=firstname, lastname=lastname, admin=False)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for('login'))
+
+    return render_template('registration.html')
+
+
+@app.route('/users', methods=['GET', 'POST'])
+@login_required
+def users():
+    # Delete User
+    if request.method == 'POST':
+        if not current_user.admin:
+            print('Not an admin')
+            return redirect(url_for('users'))
+
+        user_id = request.form.get('user_id')
+        user = User.query.get(user_id)
+        if user and user.admin == False:
+            db.session.delete(user)
+            db.session.commit()
+            return redirect(url_for('users'))
+
+    unsorted_users = User.query.all()
+    users = sorted(unsorted_users, key=lambda user: user.firstname.casefold())
+    return render_template('users.html', users=users, loggedIn=True)
+
+@app.route('/upgrade', methods=['POST'])
+@login_required
+def userToAdmin():
+    if not current_user.admin:
+        print('Not an admin')
+        return redirect(url_for('users'))
+
+    user_id = request.form.get('user_id')
+    user = User.query.get(user_id)
+    if user and user.admin == False:
+        user.admin = True
+        db.session.commit()
+    return redirect(url_for('user', user_id=user_id))
+
+@app.route('/downgrade', methods=['POST'])
+@login_required
+def adminToUser():
+    if not current_user.admin:
+        print('Not an admin')
+        return redirect(url_for('users'))
+
+    user_id = request.form.get('user_id')
+    user = User.query.get(user_id)
+    if user and user.admin == True:
+        user.admin = False
+        db.session.commit()
+    return redirect(url_for('user', user_id=user_id))
+
+@app.route('/user/<int:user_id>', methods=['GET'])
+@login_required
+def user(user_id):
+    user = User.query.get_or_404(user_id)
+    return render_template('user.html', user=user, loggedIn=True)
+
+@app.route('/pwreset', methods=['POST'])
+@login_required
+def pwreset():
+    user_id = int(request.form.get('user_id'))
+    if current_user.admin or current_user.id == user_id:
+        newPw = request.form.get('newPw')
+        user = User.query.get_or_404(user_id)
+        user.password = bcrypt.generate_password_hash(newPw).decode('utf-8')
+        db.session.commit()
+    else:
+        print('Not an admin')
+    return redirect(url_for('users'))
+    
+        
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    # app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
